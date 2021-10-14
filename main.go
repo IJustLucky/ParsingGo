351ad4f110b312ff22e56c9fcf9d69c37d2ccaec
IJustLucky <87260716+IJustLucky@users.noreply.github.com>
2021-10-14 16:08:22 -0400

    Update main.go
    Now the data is read and sent to localhost.

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
+
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 )
 
 type trade struct {
@@ -41,6 +47,15 @@ func parseCSVFile(path string) (trades []trade, _ error) {
 		price, err := strconv.ParseFloat(pricestr, 64)
 		if err != nil {
 			return nil, err
+		}
+
+		n := len(trades)
+		var total float64
+		sumArr := []float64{}
+		for i := 0; i < n; i++ {
+			totalArr := append(sumArr, price)
+			total += totalArr[len(totalArr)-1]
+			fmt.Println(total / float64(i))
 		}
 
 		qtyStr := splitValues[2]
@@ -84,4 +99,55 @@ func parseCSVFile(path string) (trades []trade, _ error) {
 		})
 	}
 	return trades, nil
+}
+
+func averagePrice(trades []trade, n int) float64 {
+	var total float64
+	for i := 0; i < n; i++ {
+		t := trades[i]
+		total += t.price
+	}
+	return total / float64(n)
+}
+
+func FloatToString(input_num float64) string {
+	return strconv.FormatFloat(input_num, 'f', 6, 64)
+}
+
+/*func avgPrice(prices []float64, n int) float64 {
+	var total float64
+	for _, v := range prices {
+		total += v
+	}
+	return total / float64(n)
+}*/
+
+func main() {
+	http.HandleFunc("/", httpserver)
+	http.ListenAndServe(":8081", nil)
+}
+
+func httpserver(w http.ResponseWriter, _ *http.Request) {
+	result := display("E:/ETHUSDTCSV/ETHUSDT-trades-2017-08.csv")
+	line := charts.NewLine()
+	line.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+		charts.WithTitleOpts(opts.Title{
+			Title: "Displaying data in Golang",
+		}))
+	line.SetXAxis([]string{"time"}).
+		AddSeries("Category A", result).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+	line.Render(w)
+}
+
+func display(path string) []opts.LineData {
+	items := make([]opts.LineData, 0)
+	trades, err := parseCSVFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	answer := averagePrice(trades, len(trades))
+	items = append(items, opts.LineData{Value: answer})
+	return items
 }
